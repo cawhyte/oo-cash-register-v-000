e8226d31ee3bb0a92ffed8dc621c696b851e8e40
Claribel Aguilar <claribel.aguilarwhyte@gmail.com>
2019-05-23 03:56:07 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -2,7 +2,7 @@ require 'pry' #This allows me to run parts of my code without messing up other p
 
 class CashRegister #classes are created by using CAPITAL letters. This also allows me to call and create new instances in my method
 attr_accessor :total :discount :price :items #allows me to access the methods @total @discount @price @items
-  def initialize(discount = 0) 
+  def initialize(discount = 0)
     @total = 0
     @discount = discount
     @items = []
