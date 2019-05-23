3962b0fa0657721ffd6ee61f1fb1290425d43ef8
Claribel Aguilar <claribel.aguilarwhyte@gmail.com>
2019-05-23 03:56:01 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,8 +1,13 @@
-class CashRegister
-attr_accessor :total
+require 'pry' #This allows me to run parts of my code without messing up other parts
 
-  def initialize(@total = 0)
-    @total = total
+class CashRegister #classes are created by using CAPITAL letters. This also allows me to call and create new instances in my method
+attr_accessor :total :discount :price :items #allows me to access the methods @total @discount @price @items
+  def initialize(discount = 0) 
+    @total = 0
+    @discount = discount
+    @items = []
+end
+
 
 
   end
